795419c9c989249184ba9ef7ffb1adeccdf9e837
Fatima-Mujahid <fmujahid.bese19seecs@seecs.edu.pk>
2021-11-26 10:24:36 +0500

    feat(categories) create categories page

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// $(document).ready(function () {
+//   if (window.innerWidth < 768) {
+//     console.log('Hello');
+//     $('.row div').removeClass('col-4');
+//   }
+// });
+
+// $(window)
+//   .on('resize', function () {
+//     var size = $(window).width(); //get updated width when window is resized
+//     $('.row div').toggleClass('col-4', size > 768); //remove class only in less or equal to 1067
+//   })
+//   .resize(); //trigger resize on load
