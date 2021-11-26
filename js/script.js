2d48b6c8a140e72bbef6ebb7db26195b525ac59f
Fatima-Mujahid <fmujahid.bese19seecs@seecs.edu.pk>
2021-11-26 19:48:25 +0500

    feat(home-categories) create categories section for home page

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
+$(document).ready(function () {
+  $('.row-scroll').slick({
+    accessibility: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    centerMode: true,
+    variableWidth: true,
+  });
+});
+
 // $(document).ready(function () {
 //   if (window.innerWidth < 768) {
 //     console.log('Hello');
@@ -11,3 +22,10 @@
 //     $('.row div').toggleClass('col-4', size > 768); //remove class only in less or equal to 1067
 //   })
 //   .resize(); //trigger resize on load
+
+// $(document).ready(function () {
+//   var outerContent = $('.row-scroll');
+//   var innerContent = $('.row-scroll > div');
+
+//   outerContent.scrollLeft((innerContent.width() - outerContent.width()) / 2);
+// });
