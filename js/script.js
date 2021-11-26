7ac570d83aa9fd73a91912076620c9f2dd952f78
Fatima-Mujahid <fmujahid.bese19seecs@seecs.edu.pk>
2021-11-26 22:14:40 +0500

    feat(categories) make category rows scrollable

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+$(function () {
   $('.row-scroll').slick({
     accessibility: true,
     infinite: true,
@@ -9,23 +9,8 @@ $(document).ready(function () {
   });
 });
 
-// $(document).ready(function () {
-//   if (window.innerWidth < 768) {
-//     console.log('Hello');
-//     $('.row div').removeClass('col-4');
-//   }
-// });
-
-// $(window)
-//   .on('resize', function () {
-//     var size = $(window).width(); //get updated width when window is resized
-//     $('.row div').toggleClass('col-4', size > 768); //remove class only in less or equal to 1067
-//   })
-//   .resize(); //trigger resize on load
-
-// $(document).ready(function () {
-//   var outerContent = $('.row-scroll');
-//   var innerContent = $('.row-scroll > div');
-
-//   outerContent.scrollLeft((innerContent.width() - outerContent.width()) / 2);
-// });
+$(function () {
+  var row = $('#categories .row');
+  var scrollto = row.offset().left + row.width() / 5;
+  row.scrollLeft(scrollto);
+});
