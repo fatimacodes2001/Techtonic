ebeac679c2b11393c988dcf93e2643406848a9b3
Fatima Mujahid <fmujahid.bese19seecs@seecs.edu.pk>
2021-12-19 13:49:15 +0500

    Revert "Laravel Breeze"

--- techtonic/routes/web.php
+++ techtonic/routes/web.php
@@ -14,11 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
-
-require __DIR__.'/auth.php';
