31e35850275df4e0da6f78d5536d012596d4c0b3
Fatima Tuz Zehra <57491064+fatimacodes2001@users.noreply.github.com>
2021-12-20 19:05:20 +0500

    added all models

---
+++ techtonic/app/Models/Address.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Address extends Model
+{
+    use HasFactory;
+    protected $table = 'addresses';
+
+    public $timestamps = false;
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'address_id');
+    }
+
+
+
+}
