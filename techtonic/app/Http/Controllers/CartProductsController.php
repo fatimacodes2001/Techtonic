74857910f582950404fd2bdf2556f78d63192bc1
Fatima-Mujahid <fmujahid.bese19seecs@seecs.edu.pk>
2021-12-28 20:51:40 +0500

    feat(controllers) add update(add to cart) route in cart controller

--- techtonic/app/Http/Controllers/CartProductsController.php
+++ techtonic/app/Http/Controllers/CartProductsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Models\Cart;
+use App\Models\Product;
 
 
 
@@ -20,5 +21,23 @@ class CartProductsController extends Controller
         
         return view('cart-items', ['products' => $prods]);
 
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $productId
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $productId)
+    {
+        $user = User::find("fatima@abc.com");
+        $cart = Cart::where("customer_email",$user->email)->first();
+        $prods = $cart->products;
+        $prod = Product::find($productId);
+        $cart->products()->attach($prod, array('quantity' => 1));
+
+        return redirect('cart');
     }
 }
