5121b68d00c3addc281fccf8807634cdc0013b44
Fatima-Mujahid <fmujahid.bese19seecs@seecs.edu.pk>
2022-01-02 10:22:15 +0500

    style(admin) make admin panel responsive

--- techtonic/resources/views/admin/categories.blade.php
+++ techtonic/resources/views/admin/categories.blade.php
@@ -13,40 +13,45 @@
         <a href="{{route('admin.categories.create')}}">Create New Category</a> 
     </div>
 
-    <table class="table table-striped table-hover">
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>Pic</th>
-                <th>Name</th>
-                <th>Update</th>
-                <th>Products</th>
-            </tr>
-        </thead>
-        <tbody>
-
-            @foreach ($categories as $category)
-
+    <div class="wrapper">
+        <table class="table table-striped table-hover">
+            <thead>
                 <tr>
-                    <td>{{ $category->id }}</td>
-                    <td><img width="65px" height="70px" src="{{ asset('storage/' . $category->pic_path) }}"></td>
-                    <td>{{ $category->name }}</td>
-                    <td>
-                        <a href="{{route('admin.categories.edit', $category->id)}}">Edit</a> | 
-                        <form class="d-inline" method="POST" action="{{ route('admin.categories.destroy', $category->id)}}">
-                            @csrf
-                            @method('DELETE')
-                            <button class="btn btn-link" onClick="return confirm('Are you sure you want to delete?')">Delete</button>
-                        </form>
-                    </td>
-                    <td><a href="{{route('admin.categories.show', $category->id)}}">View Products</a></td>
-                </tr>  
+                    <th>ID</th>
+                    <th>Pic</th>
+                    <th>Name</th>
+                    <th>Update</th>
+                    <th>Products</th>
+                </tr>
+            </thead>
+            <tbody>
 
-            @endforeach
-                
-        </tbody>
-    
-    </table>
+                @forelse ($categories as $category)
+
+                    <tr>
+                        <td>{{ $category->id }}</td>
+                        <td><img class="img-x-sm" src="{{ asset('storage/' . $category->pic_path) }}"></td>
+                        <td>{{ $category->name }}</td>
+                        <td>
+                            <a href="{{route('admin.categories.edit', $category->id)}}" class="green">Edit</a> | 
+                            <form class="d-inline" method="POST" action="{{ route('admin.categories.destroy', $category->id)}}">
+                                @csrf
+                                @method('DELETE')
+                                <button class="btn btn-link red" onClick="return confirm('Are you sure you want to delete?')">Delete</button>
+                            </form>
+                        </td>
+                        <td><a href="{{route('admin.categories.show', $category->id)}}">View Products</a></td>
+                    </tr>  
+
+                @empty
+                    <div class="alert alert-primary" role="alert">
+                        <p class="mb-0">No categories have been added yet.</p> 
+                    </div>
+                @endforelse
+                    
+            </tbody>
+        </table>
+    </div>
    
 @endsection
 
